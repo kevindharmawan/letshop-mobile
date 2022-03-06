bc10af8fb3542851d645d05ec31dc94214a91a34
Cheapfan <stevencokro32@gmail.com>
2022-03-06 17:25:35 +0700

    social button dan button

--- lib/shared/buttons/button.dart
+++ lib/shared/buttons/button.dart
@@ -1,1 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:letshop_mobile/utils/constants/font_size.dart';
+import 'package:letshop_mobile/utils/device/sizing.dart';
+
+class Button extends StatelessWidget {
+
+  final Function onPress;
+
+  const Button({
+    Key? key,
+    required this.onPress
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return OutlinedButton(
+      style: OutlinedButton.styleFrom(
+        primary: Colors.white,
+        backgroundColor: Colors.blueAccent,
+        padding: EdgeInsets.symmetric(
+          vertical: Sizing.h(8),
+          horizontal: Sizing.w(16),
+        ),
+      ),
+      onPressed: () {},
+      child: Text(
+        'Login',
+        style: TextStyle(
+            fontSize: FontSize.bodyRegular
+        ),
+      ),
+    );
+  }
+}
+
 
--- lib/shared/buttons/social_button.dart
+++ lib/shared/buttons/social_button.dart
@@ -1,1 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:letshop_mobile/utils/constants/font_size.dart';
+import 'package:letshop_mobile/utils/device/sizing.dart';
 
+
+
+class SocialButton extends StatelessWidget {
+  final Function onPress;
+
+  const SocialButton({
+    Key? key,
+    required this.onPress,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return OutlinedButton.icon(
+      style: OutlinedButton.styleFrom(
+        primary: Colors.black,
+        padding: EdgeInsets.symmetric(
+          vertical: Sizing.h(8),
+          horizontal: Sizing.w(16),
+        ),
+      ),
+      onPressed: (){},
+      icon: Image.network('https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/1200px-Google_%22G%22_Logo.svg.png', height: Sizing.h(16),width: Sizing.w(16),),
+      label: Text(
+        'Login with Google',
+        style: TextStyle(
+            fontSize: FontSize.bodyRegular
+        ),
+      ),
+    );
+  }
+}
